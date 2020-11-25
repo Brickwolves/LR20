c2d3b4a562b588d8565a1cdb7ab2ab570c4ef425
Cain Smith <xlqgik@gmail.com>
2020-11-25 14:04:14 -0500

    Changed imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/RingDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/RingDetector.java
@@ -1,12 +1,7 @@
 package org.firstinspires.ftc.teamcode.Vision;
 
 // Vision Hardware
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-import org.firstinspires.ftc.teamcode.Utilities.Utils;
 
 public class RingDetector {
 
