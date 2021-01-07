2b8eac78070910fc429afad92336b2af36dfd00e
Gordon Gabbay <GGabbay@Gordons-NEW-MacBook-Air.local>
2021-01-07 11:22:09 -0500

    AlphaVisionDash uses FTCDashboard to modify cropping area

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/DashConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/DashConstants.java
@@ -9,6 +9,15 @@ public class DashConstants {
     public static double d = .0004;
 
 
+    /* ------- VISION --------- */
+    // Top rectangle starting percentages
+    public static double rectTopX1Percent = 0; public static double rectTopX2Percent = 0;
+    public static double rectTopY1Percent = 0; public static double rectTopY2Percent = 0;
+
+    // Bottom rectangle starting percentages
+    public static double rectBottomX1Percent = 0; public static double rectBottomX2Percent = 0;
+    public static double rectBottomY1Percent = 0; public static double rectBottomY2Percent = 0;
+
 
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/AlphaVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/AlphaVision.java
@@ -1,67 +1,111 @@
 package org.firstinspires.ftc.teamcode.Vision;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Hardware.MecanumRobot;
 import org.firstinspires.ftc.teamcode.Utilities.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.*;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
 
-import static android.os.SystemClock.sleep;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 
-@Autonomous(name = "WebCamStackDetectorAuto", group = "Autonomous")
-public class WebCamStackDetector extends OpMode {
+@Config
+@TeleOp(name="AlphaVision", group="TeleOp Linear Opmode")
+public class AlphaVision extends LinearOpMode
+{
+    private MecanumRobot mecanumRobot;
 
-    OpenCvCamera webCam = null;
-    static double ringCount = 0;
+    OpenCvCamera webcam;
+    private static double ringCount = 0;
 
-    @Override
-    public void init(){
+    private FtcDashboard dashboard = FtcDashboard.getInstance();
+    private Telemetry dashboardTelemetry = dashboard.getTelemetry();
+    private MultipleTelemetry multTelemetry = new MultipleTelemetry(telemetry, dashboardTelemetry);
+
+    public void initialize(){
         Utils.setOpMode(this);
+        mecanumRobot = new MecanumRobot();
     }
 
     @Override
-    public void init_loop(){
+    public void runOpMode()
+    {
 
-        // Init the phone
-        int cameraMonitorViewId = Utils.hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        webCam = OpenCvCameraFactory.getInstance().createWebcam(Utils.hardwareMap.get(WebcamName.class, "webcam"), cameraMonitorViewId);
 
-        webCam.setPipeline(new RingDetectingPipeline());
-        // Start Streaming
-        webCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+        /*
+        Set up camera, and pipeline
+         */
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam"), cameraMonitorViewId);
+
+        webcam.setPipeline(new RingDetectingPipeline());
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
             @Override
-            public void onOpened() {
-
-                webCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            public void onOpened()
+            {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
             }
         });
+
+
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+        waitForStart();
+
+
+        /*
+        ACTION
+         */
+        while (opModeIsActive())
+        {
+
+            if (ringCount == 1.0){
+                // drive somewhere
+                Utils.telemetry.addData("RingCount", 1.0);
+            }
+            else if (ringCount == 4.0){
+                // drive somewhere else
+                Utils.telemetry.addData("RingCount", 4.0);
+            }
+            else {
+                // drive somewhere other than else
+                Utils.telemetry.addData("RingCount", 0.0);
+            }
+
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+            telemetry.update();
+
+            if(gamepad1.a)
+            {
+                webcam.stopStreaming();
+            }
+            sleep(100);
+        }
     }
 
-    @Override
-    public void loop() {
-
-        if (ringCount == 1.0){
-            // drive somewhere
-            Utils.telemetry.addData("RingCount", 1.0);
-        }
-        else if (ringCount == 4.0){
-            // drive somewhere else
-            Utils.telemetry.addData("RingCount", 4.0);
-        }
-        else {
-            // drive somewhere other than else
-            Utils.telemetry.addData("RingCount", 0.0);
-        }
-
-    }
-
-    class RingDetectingPipeline extends OpenCvPipeline {
+    class RingDetectingPipeline extends OpenCvPipeline
+    {
+        boolean viewportPaused;
 
         // Init mats here so we don't repeat
         Mat YCbCr = new Mat();
@@ -77,11 +121,10 @@ public class WebCamStackDetector extends OpMode {
         int rectBottomX1; int rectBottomX2;     double rectBottomX1Percent = 0; double rectBottomX2Percent = 0;
         int rectBottomY1; int rectBottomY2;     double rectBottomY1Percent = 0; double rectBottomY2Percent = 0;
 
-        final double[] ORANGE = {  };
 
         @Override
-        public Mat processFrame(Mat input) {
-
+        public Mat processFrame(Mat input)
+        {
             // Convert
             Imgproc.cvtColor(input, YCbCr, Imgproc.COLOR_RGB2YCrCb);
 
@@ -134,16 +177,16 @@ public class WebCamStackDetector extends OpMode {
             if (
 
                     finalLowerAve > 15 &&
-                    finalLowerAve < 130 &&
-                    finalUpperAve < 130
+                            finalLowerAve < 130 &&
+                            finalUpperAve < 130
 
             ) ringCount = 4.0;
             else if (
 
                     finalLowerAve > 10 &&
-                    finalUpperAve < 15 &&
-                    finalLowerAve > 10 &&
-                    finalUpperAve < 15
+                            finalUpperAve < 15 &&
+                            finalLowerAve > 10 &&
+                            finalUpperAve < 15
 
             ) ringCount = 0.0;
             else ringCount = 1.0;
@@ -152,6 +195,20 @@ public class WebCamStackDetector extends OpMode {
             // Return altered image
             return outPut;
         }
-    }
 
+        @Override
+        public void onViewportTapped()
+        {
+            viewportPaused = !viewportPaused;
+
+            if(viewportPaused)
+            {
+                webcam.pauseViewport();
+            }
+            else
+            {
+                webcam.resumeViewport();
+            }
+        }
+    }
 }
