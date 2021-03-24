4c9215d6896322d203d3c55edb29a49bdbade032
Cain Smith <xlqgik@gmail.com>
2021-03-23 20:11:51 -0400

    Modified Dash_RingFinder values to correct YCrCb thresholds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/DashConstants/Dash_RingFinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/DashConstants/Dash_RingFinder.java
@@ -6,14 +6,14 @@ import com.acmerobotics.dashboard.config.Config;
 public class Dash_RingFinder {
 
     // RING FINDER
-    public static int MIN_Y = 100;
-    public static int MAX_Y = 120;
+    public static int MIN_Y = 130;
+    public static int MAX_Y = 90;
 
-    public static int MIN_Cr = 165;
-    public static int MAX_Cr = 255;
+    public static int MIN_Cr = 220;
+    public static int MAX_Cr = 180;
 
-    public static int MIN_Cb = 90;
-    public static int MAX_Cb = 200;
+    public static int MIN_Cb = 80;
+    public static int MAX_Cb = 45;
 
     public static int blur = 5;
     public static int erode_const = 5;
